6ab8d1e220a32e030d9928413d697d946c67d572
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-02-04 00:33:00 -0300

    chore: enable lint rule

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -4,6 +4,6 @@ module.exports = {
         // '@next/next/no-html-link-for-pages': ['error', 'src/pages'],
         // 'turbo/no-undeclared-env-vars': 'off',
         // 'react-hooks/exhaustive-deps': 'off',
-        // 'react/self-closing-comp': ['error', { component: true, html: true }],
+        'react/self-closing-comp': ['error', { component: true, html: true }],
     },
 }
