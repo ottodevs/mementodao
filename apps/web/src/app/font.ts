4104c92a74cc4c1b46d23520c382914e8bc0f6f4
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-02-05 20:47:57 -0300

    feat: add and config tailwind with rippleui

--- apps/web/src/app/font.ts
+++
@@ -1,7 +0,0 @@
-import { Space_Grotesk } from '@next/font/google'
-
-export const font = Space_Grotesk({
-    subsets: ['latin'],
-    weight: ['400', '700'],
-    variable: '--font-grotesk',
-})
---
+++ apps/web/src/style/font.ts
@@ -0,0 +1,7 @@
+import { Inter } from '@next/font/google'
+
+export const font = Inter({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+    variable: '--font-inter',
+})
