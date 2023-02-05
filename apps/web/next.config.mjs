acf87aab9baffbf42ff7a1df40aecf6a3144933c
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-02-05 20:49:23 -0300

    chore: tidy next config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,9 +3,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    // typescript: {
-    //     tsconfigPath: './tsconfig.build.json',
-    // },
     images: {
         domains: ['avatars.dicebear.com'],
     },
