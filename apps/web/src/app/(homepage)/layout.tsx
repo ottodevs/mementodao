3e4527c3841b14ce967966db16ed46e3904df2eb
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-02-06 07:24:38 -0300

    fix: tidy layouts and add missing page

--- apps/web/src/app/(homepage)/layout.tsx
+++ apps/web/src/app/(homepage)/layout.tsx
@@ -1,5 +1,5 @@
 import { Footer } from '@/components/Footer'
-import { Navigation } from '@/components/Navigation'
+import { Navigation } from '@/components/HomeNavbar'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
--- apps/web/src/app/(webapp)/layout.tsx
+++ apps/web/src/app/(webapp)/layout.tsx
@@ -1,23 +1,19 @@
 'use client' // needed for Web3Provider
 
+import Navbar from '@/components/AppNavbar'
+import { Sidebar } from '@/components/Sidebar'
 import { Web3Provider } from '@/components/Web3Provider'
-
-// import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
-
-// import { Sidebar } from '@/components/Sidebar'
-
-// export default function EventsLayout({ children }: { children: React.ReactNode }) {
-//     return (
-//                 <Sidebar />
-
-//     )
-// }
 
 export default function WebAppLayout({ children }: { children: React.ReactNode }) {
     return (
         <Web3Provider>
-            <h1>WebAppLayout</h1>
-            {children}
+            <div className='z-10 relative sm:-8 p-4 bg-gradient-to-br to-[#ef4136bf] from-[#fbb040bf] min-h-screen flex flex-row'>
+                <Sidebar />
+                <div className='-z-10 flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
+                    <Navbar />
+                    {children}
+                </div>
+            </div>
         </Web3Provider>
     )
 }
